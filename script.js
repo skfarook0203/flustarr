b10cd8e53376ce55a2dc7f811ce0cf4b597cdb27
Shaik Farook <skfarook0203@gmail.com>
2026-01-01 13:37:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function sayHello() {
-  alert("Hello! JavaScript is working 🚀");
+const products = [
+  { name: "T-Shirt", price: 499 },
+  { name: "Shoes", price: 1999 },
+  { name: "Watch", price: 2999 }
+];
+
+function showProducts() {
+  let output = "";
+  products.forEach((product) => {
+    output += product.name + " - ₹" + product.price + "\n";
+  });
+  alert(output);
 }
