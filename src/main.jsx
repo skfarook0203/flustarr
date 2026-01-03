6f7dae79f3183d764bff1d9275ec83b1a23ad7c4
Shaik Farook <skfarook0203@gmail.com>
2026-01-04 00:34:34 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+function Test() {
+  return <h1>FLUSTAR TEST</h1>;
+}
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<Test />);
