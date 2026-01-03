e09a351b91e8f07c0e25da86d393b616c3b73645
Shaik Farook <skfarook0203@gmail.com>
2026-01-03 23:49:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Cart from "./pages/Cart";
-import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <Home />
+      <Products />
+      <Cart />
+    </div>
   );
 }
